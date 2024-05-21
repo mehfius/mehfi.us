4952a9bcfc01093c61580d83dfb103815022aebf
mehfius <matheus.ferraz@gmail.com>
2024-05-21 16:46:07 +0000

    .

--- js/start.js
+++ js/start.js
@@ -70,22 +70,22 @@ const load_html = async function(url,e){
 
     fetch(url)
     .then(response => {
-      // Verifica se a requisição foi bem-sucedida
+     
       if (!response.ok) {
         throw new Error('Falha ao carregar o arquivo HTML');
       }
-      // Retorna o conteúdo do arquivo HTML como texto
+      
       return response.text();
     })
     .then(html => {
-      // 2. Transforma o conteúdo HTML em um objeto
+    
       const parser = new DOMParser();
       const doc = parser.parseFromString(html, 'text/html');
   
-      // 3. Armazena o objeto resultante em uma variável
+     
       const objetoHTML = doc;
   
-      // Agora você pode manipular o objeto HTML conforme necessário
+
 
       e.innerHTML = '';
 
