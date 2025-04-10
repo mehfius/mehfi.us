9bc850e1fdee717aa913ed4d17804d448ff17b25
Matheus <matheus.ferraz@gmail.com>
2025-04-10 11:11:48 -0300

    Update tmux session handling and improve script usability
    - Changed comments to provide clearer descriptions of tmux commands
    - Modified tmux split-window command to target the existing session
    - Added conditional attachment to the tmux session based on terminal interactivity

--- tmux.sh
+++ tmux.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# Inicia uma nova sessão tmux chamada "mysession" com o primeiro comando
+# Cria uma nova sessão tmux chamada "mysession" com o primeiro comando
 tmux new-session -d -s mysession 'http-server /home/mehfius/pessoal/supacall_dialog/ -p 3002'
 
-# Abre um novo painel horizontal na mesma sessão com o segundo comando
-tmux split-window -h 'http-server /home/mehfius/pessoal/ -p 3006'
+# Cria um novo painel horizontal com o segundo comando
+tmux split-window -h -t mysession 'http-server /home/mehfius/pessoal/ -p 3006'
 
-# Anexa à sessão "mysession"
-tmux attach-session -t mysession
+# Se o script estiver rodando num terminal interativo, anexa à sessão
+if [ -t 1 ]; then
+  tmux attach-session -t mysession
+else
+  echo "Sessão 'mysession' criada. Use 'tmux attach -t mysession' para entrar."
+fi
